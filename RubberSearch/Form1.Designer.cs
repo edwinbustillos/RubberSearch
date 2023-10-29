6cb2bb8a8cc6ff7e486ebf4d8f155d3ce10a5d81
Edwin Bustillos <edwin@bustillos.id>
2023-10-29 11:35:23 -0300

    Adicionar arquivos de projeto.

---
+++ RubberSearch/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿using System;
+
+namespace RubberSearch
+{
+        //Me ajude a corrigir este arquivo        //Me ajude a corrigir este arquivo
+    partial class Form1
+    {
+        /// <sumário>
+        /// Variável de designer necessária.
+        /// </sumário>
+
+        /// <sumário>
+        /// Limpar os recursos que estão sendo usados.
+        /// </sumário>
+        /// <param name = "disposing"> true se os recursos//Me ajude a corrigir este arquivo
+        private void InitializeComponent()
+        {
+            MaterialWinforms.Controls.MaterialRaisedButton materialRaisedButton2;
+            System.Drawing.Drawing2D.GraphicsPath graphicsPath1 = new System.Drawing.Drawing2D.GraphicsPath();
+            System.Drawing.Drawing2D.GraphicsPath graphicsPath2 = new System.Drawing.Drawing2D.GraphicsPath();
+            System.Windows.Forms.Label label1;
+            this.materialProgressBar1 = new MaterialWinforms.Controls.MaterialProgressBar();
+            this.materialTextBox1 = new MaterialWinforms.Controls.MaterialTextBox();
+            this.materialRaisedButton1 = new MaterialWinforms.Controls.MaterialRaisedButton();
+            this.materialTextBox2 = new MaterialWinforms.Controls.MaterialTextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            materialRaisedButton2 = new MaterialWinforms.Controls.MaterialRaisedButton();
+            label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // materialRaisedButton2
+            // 
+            materialRaisedButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            materialRaisedButton2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            materialRaisedButton2.CausesValidation = false;
+            materialRaisedButton2.Depth = 0;
+            materialRaisedButton2.Elevation = 5;
+            materialRaisedButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            materialRaisedButton2.Location = new System.Drawing.Point(168, 26);
+            materialRaisedButton2.MouseState = MaterialWinforms.MouseState.HOVER;
+            materialRaisedButton2.Name = "materialRaisedButton2";
+            materialRaisedButton2.Primary = true;
+            graphicsPath1.FillMode = System.Drawing.Drawing2D.FillMode.Alternate;
+            materialRaisedButton2.ShadowBorder = graphicsPath1;
+            materialRaisedButton2.Size = new System.Drawing.Size(76, 45);
+            materialRaisedButton2.TabIndex = 9;
+            materialRaisedButton2.TabStop = false;
+            materialRaisedButton2.Text = "Indexar";
+            materialRaisedButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            materialRaisedButton2.UseMnemonic = false;
+            materialRaisedButton2.UseVisualStyleBackColor = false;
+            materialRaisedButton2.Click += new System.EventHandler(this.materialRaisedButton2_Click);
+            // 
+            // materialProgressBar1
+            // 
+            this.materialProgressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialProgressBar1.Depth = 0;
+            this.materialProgressBar1.InvertedProgressBar = false;
+            this.materialProgressBar1.Location = new System.Drawing.Point(3, 450);
+            this.materialProgressBar1.Maximum = 100;
+            this.materialProgressBar1.Minimum = 0;
+            this.materialProgressBar1.MouseState = MaterialWinforms.MouseState.HOVER;
+            this.materialProgressBar1.Name = "materialProgressBar1";
+            this.materialProgressBar1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.materialProgressBar1.Size = new System.Drawing.Size(712, 5);
+            this.materialProgressBar1.Step = 100;
+            this.materialProgressBar1.Style = MaterialWinforms.Controls.MaterialProgressBar.ProgressStyle.Determinate;
+            this.materialProgressBar1.TabIndex = 1;
+            this.materialProgressBar1.Text = "materialProgressBar1";
+            this.materialProgressBar1.Value = 100;
+            this.materialProgressBar1.Click += new System.EventHandler(this.materialProgressBar1_Click);
+            // 
+            // materialTextBox1
+            // 
+            this.materialTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialTextBox1.Depth = 0;
+            this.materialTextBox1.Hint = "Digite sua busca:";
+            this.materialTextBox1.Location = new System.Drawing.Point(245, 26);
+            this.materialTextBox1.MaxLength = 2147483647;
+            this.materialTextBox1.MouseState = MaterialWinforms.MouseState.HOVER;
+            this.materialTextBox1.Name = "materialTextBox1";
+            this.materialTextBox1.ReadOnly = false;
+            this.materialTextBox1.SelectedText = "";
+            this.materialTextBox1.SelectionLength = 0;
+            this.materialTextBox1.SelectionStart = 0;
+            this.materialTextBox1.Size = new System.Drawing.Size(386, 45);
+            this.materialTextBox1.TabIndex = 3;
+            this.materialTextBox1.TabStop = false;
+            // 
+            // materialRaisedButton1
+            // 
+            this.materialRaisedButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialRaisedButton1.Depth = 0;
+            this.materialRaisedButton1.Elevation = 5;
+            this.materialRaisedButton1.Location = new System.Drawing.Point(632, 26);
+            this.materialRaisedButton1.MouseState = MaterialWinforms.MouseState.HOVER;
+            this.materialRaisedButton1.Name = "materialRaisedButton1";
+            this.materialRaisedButton1.Primary = true;
+            graphicsPath2.FillMode = System.Drawing.Drawing2D.FillMode.Alternate;
+            this.materialRaisedButton1.ShadowBorder = graphicsPath2;
+            this.materialRaisedButton1.Size = new System.Drawing.Size(83, 45);
+            this.materialRaisedButton1.TabIndex = 4;
+            this.materialRaisedButton1.Text = "Buscar";
+            this.materialRaisedButton1.UseVisualStyleBackColor = true;
+            this.materialRaisedButton1.Click += new System.EventHandler(this.materialRaisedButton1_Click);
+            // 
+            // materialTextBox2
+            // 
+            this.materialTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialTextBox2.Depth = 0;
+            this.materialTextBox2.Hint = "";
+            this.materialTextBox2.Location = new System.Drawing.Point(3, 3);
+            this.materialTextBox2.MaxLength = 2147483647;
+            this.materialTextBox2.MouseState = MaterialWinforms.MouseState.HOVER;
+            this.materialTextBox2.Name = "materialTextBox2";
+            this.materialTextBox2.Padding = new System.Windows.Forms.Padding(5);
+            this.materialTextBox2.ReadOnly = false;
+            this.materialTextBox2.SelectedText = "";
+            this.materialTextBox2.SelectionLength = 0;
+            this.materialTextBox2.SelectionStart = 0;
+            this.materialTextBox2.Size = new System.Drawing.Size(534, 367);
+            this.materialTextBox2.TabIndex = 6;
+            this.materialTextBox2.TabStop = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(118)))), ((int)(((byte)(210)))));
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 74);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.splitContainer1.Panel2.Controls.Add(this.materialTextBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(712, 375);
+            this.splitContainer1.SplitterDistance = 166;
+            this.splitContainer1.TabIndex = 10;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(158, 371);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(118)))), ((int)(((byte)(210)))));
+            label1.CausesValidation = false;
+            label1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label1.ForeColor = System.Drawing.Color.White;
+            label1.Image = global::RubberSearch.Properties.Resources.logo2;
+            label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            label1.Location = new System.Drawing.Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(166, 71);
+            label1.TabIndex = 8;
+            label1.Text = "RubberSearch";
+            label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(718, 463);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(materialRaisedButton2);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.materialRaisedButton1);
+            this.Controls.Add(this.materialTextBox1);
+            this.Controls.Add(this.materialProgressBar1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+        private MaterialWinforms.Controls.MaterialProgressBar materialProgressBar1;
+        private MaterialWinforms.Controls.MaterialTextBox materialTextBox1;
+        private MaterialWinforms.Controls.MaterialRaisedButton materialRaisedButton1;
+        private MaterialWinforms.Controls.MaterialTextBox materialTextBox2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
---
+++ RubberSearch/Form1.cs
@@ -0,0 +1,199 @@
+﻿using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.QueryParsers.Classic;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using MaterialWinforms;
+using MaterialWinforms.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace RubberSearch
+{
+    public partial class Form1 : MaterialForm
+    {
+        private readonly string indexDir = @"C:\RubberSearch\Index";
+        private readonly string dataDir = @"C:\RubberSearch\Data";
+
+        public Form1()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue700, Primary.Blue700, Primary.Blue700, Accent.LightBlue200, TextShade.WHITE);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!System.IO.Directory.Exists(indexDir))
+            {
+                // Se não existir, criar a pasta1
+                System.IO.Directory.CreateDirectory(indexDir);
+            }
+
+            // Verificar se a pasta2 existe
+            if (!System.IO.Directory.Exists(dataDir))
+            {
+                // Se não existir, criar a pasta2
+                System.IO.Directory.CreateDirectory(dataDir);
+            }
+
+        }
+
+        private void materialRaisedButton2_Click(object sender, EventArgs e)
+        {
+            IndexFiles();
+            MessageBox.Show("Indexação concluída com sucesso!");
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IndexFiles()
+        {
+            if (System.IO.Directory.Exists(indexDir))
+            {
+                System.IO.Directory.Delete(indexDir, true);
+            }
+            var dirInfo = new DirectoryInfo(dataDir);
+            var files = dirInfo.GetFiles("*.txt", SearchOption.AllDirectories);
+
+            using (var dir = FSDirectory.Open(new DirectoryInfo(indexDir)))
+            using (var analyzer = new StandardAnalyzer(Lucene.Net.Util.LuceneVersion.LUCENE_48))
+            using (var writer = new IndexWriter(dir, new IndexWriterConfig(Lucene.Net.Util.LuceneVersion.LUCENE_48, analyzer)))
+            {
+                foreach (var file in files)
+                {
+                    var doc = new Document();
+                    doc.Add(new TextField("path", file.FullName, Field.Store.YES));
+                    doc.Add(new TextField("contents", File.ReadAllText(file.FullName), Field.Store.YES));
+                    writer.AddDocument(doc);
+                }
+
+                writer.Flush(true, true);
+                writer.Commit();
+            }
+        }
+
+        [Obsolete]
+        private List<SearchResult> SearchIndex(string query)
+        {
+            FSDirectory dir = FSDirectory.Open(new DirectoryInfo(indexDir));
+            
+            // Criar um analisador padrão
+            StandardAnalyzer analyzer = new StandardAnalyzer(Lucene.Net.Util.LuceneVersion.LUCENE_48);
+            
+            IndexReader reader = IndexReader.Open(dir, 1);
+             
+            IndexSearcher searcher = new IndexSearcher(reader);
+
+            var results = new List<SearchResult>();
+
+            {  
+                // Criar um parser de consulta para o campo "contents"
+                var parser = new QueryParser(Lucene.Net.Util.LuceneVersion.LUCENE_48, "contents", analyzer);
+
+                //Escapar caracteres especiais na consulta
+                var escaped_query = QueryParserBase.Escape(query);
+
+                // Criar uma consulta fuzzy com o termo "featured" e a similaridade mínima de 0.7
+                //var luceneQuery = parser.GetFuzzyQuery("contents", escaped_query, 0.7f);
+               
+               Query luceneQuery = parser.Parse(QueryParser.Escape(escaped_query));
+
+                var hits = searcher.Search(luceneQuery, 1000);
+
+                for (int i = 0; i < hits.ScoreDocs.Length; ++i)
+                {
+                    ScoreDoc scoreDoc = hits.ScoreDocs[i];
+
+                    int docId = scoreDoc.Doc;
+                    //float score = scoreDoc.Score;
+
+                    Document doc = searcher.Doc(docId);
+                    var path = doc.Get("path");
+                    var content = File.ReadAllText(path);
+                    results.Add(new SearchResult { Path = path, Content = content });
+                }
+            }      
+            return results;
+        }
+
+        public class SearchResult
+        {
+            public string Path { get; set; }
+            public string Content { get; set; }
+        }
+
+        [Obsolete]
+        private void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            string query = materialTextBox1.Text;
+
+            if (string.IsNullOrEmpty(query))
+            {
+                MessageBox.Show("Por favor, informe um valor de busca.");
+                return;
+            }
+
+            var results = SearchIndex(query);
+
+            if (results.Any())
+            {
+                listView1.Items.Clear();
+
+                foreach (var result in results)
+                {
+                    var item = new ListViewItem(result.Path);
+                    item.Tag = result.Content;
+                    item.Text = Path.GetFileNameWithoutExtension(result.Path);
+                    Console.WriteLine(result);
+                    listView1.Items.Add(item);
+                }
+
+                MessageBox.Show($"Foram encontrados {results.Count} resultados para a busca.");
+
+            }
+            else
+            {
+                MessageBox.Show("Nenhum resultado encontrado para a busca informada.");
+            }
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                var content = listView1.SelectedItems[0].Tag.ToString();
+                materialTextBox2.Clear();
+                materialTextBox2.Text = content;
+            }
+        }
+
+        private void materialProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
